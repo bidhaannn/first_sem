cacdffe6766476f7084e09b7d537667182a00f83
bidhaannn <161105400+bidhaannn@users.noreply.github.com>
2024-02-24 22:12:47 +0545

    first

---
+++ practice.py
@@ -0,0 +1,172 @@
+# print(('hello hython'.title()))
+# print('Hello'<'hello')
+
+# S = 'The World is Beautiful'
+# x = S.split()
+# print(x)
+
+# S = '  The    World is  Beautiful'
+# x = S.split()
+# print(x)
+
+# S = 'The\n\rWorld\tis Beautiful'
+# x = S.split()
+# print(x)
+
+# S = 'red,green,blue'
+# x = S.split('g')
+# print(x)
+
+# S = 'the beginning is the end is the be'
+# x = S.split(' is ')
+# print(x)
+
+# S = 'The World is Beautiful isnt it'
+# x = S.split(None,4)
+# print(x)
+
+# S = 'First line\nSecond line'
+# x = S.splitlines()
+# print(x)
+
+# S = 'Hello, World!'
+# x = S.swapcase()
+# print(x)
+
+# S = 'is my FAVOURITE number.'
+# x = S.capitalize()
+# print(x)
+
+# S = 'bob is a CEO at ABC.'
+# x = S.capitalize()
+# print(x)
+
+# S = 'Centered'
+# x = S.center(14, '*')
+# print(x)
+
+# S = '{} is {} years old.'.format(25, 'Bob')
+# print(S)
+
+# S = 'Long, Longer, Longest'
+# x = S.replace('Long','Small',3)
+# print(x)
+
+# S = {'red', 'green', 'blue'}
+# S.add('yellow')
+# print(S)
+
+# S = {'red', 'green', 'blue'}
+# S.add([1, 2])
+
+# list=[['a','b','c'],[3,2,1]]
+# print(list[0])
+
+# for i in range(1,11):
+#     if i==3:
+#         break
+#     print(i)
+#     i=i+1
+# else:
+#     print("this is inside else of for loop")
+
+# i=0
+# while i<6:
+#     if i==3:
+#         continue
+#     print(i)
+#     i+=1
+
+# print(bin(20))
+
+# a=2+3j
+# print(a.imag,a.real)
+
+# print("python",end="/","programming")
+
+# fruits = [1,2,3,4,5,6]
+# fruits.pop(fruits[3])
+# print(fruits)
+
+# car = {"brand": "Ford", "model": "Mustang",
+# "year": 1964}
+# car.pop("mustang")
+# print(car)
+
+# print('hi','hello','hey',end='2')
+
+# a={1,2,3,4}
+# a.add((1,2))
+# print(a)
+
+# lst=[1,2,3,4]
+# print(lst[0:7])
+
+# a={1:2,2:3,3:4}
+# a[1]=5
+# print(a)
+
+# a=5
+# b=2
+# x= a>b and not(a==b)
+# y=a**3
+# z=a//b
+# t=1,2,3,4
+# print(type(x))
+# print(type(y))
+# print(type(z))
+# print(type(t))
+
+# a={1,2,3,4}
+# a.remove(5)
+# print(a)
+
+# print([1,2],[1,2])
+
+# a={1,2,3,4}
+# print(a[2])
+
+# print('hello \npython' )
+
+# a='bidhan'
+# b='koirala'
+# print(f'hello i am {0} {1}') 
+
+# print('hello'+str(6))
+
+# print(type(5/2))
+
+# l=[1,2]
+# print(l*1)
+
+# x='abc'
+# for i in range(x):
+#     print(i)
+
+# z=max(2,3,5)+10
+# print(z)
+
+# z=max(10,15,20)-10
+# print(z)
+
+# z=min(10,20,30)-5
+# print(z)
+
+# a=[1,2,3,4]
+# a.append(5)
+# print(a)
+
+# a={1:5,2:3,3:4}
+# c=a.get(5)
+# print(c)
+
+try:
+    pass
+except:
+    pass
+else:
+    pass
+
+a=(1,2,3,4)
+a.add(5)
+print(a)
